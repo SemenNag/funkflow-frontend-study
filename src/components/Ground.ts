d3187e4275fffdf548bb55b08431ba7e0dc7543f
mrVandermond <kirill.guyda32@gmail.com>
2025-01-28 18:12:35 +0100

    fixed rendering Ground

--- src/components/Ground.ts
+++ src/components/Ground.ts
@@ -1,4 +1,13 @@
-import { BufferGeometry, Line, LineBasicMaterial, Mesh, MeshBasicMaterial, PlaneGeometry, Scene, Vector3 } from 'three';
+import {
+  BufferGeometry,
+  Line,
+  LineBasicMaterial,
+  Mesh,
+  MeshBasicMaterial,
+  PlaneGeometry,
+  Scene,
+  Vector3
+} from 'three';
 
 export class Ground {
   private readonly width: number;
@@ -15,22 +24,24 @@ export class Ground {
     const planeGeometry = new PlaneGeometry(this.width, this.height);
     const planeMaterial = new MeshBasicMaterial({
       color: 0xf8f9fa,
-      wireframe: true,
     });
 
     const plane = new Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = - Math.PI / 2;
+    plane.position.set(0, 0, 0);
+    plane.rotation.set(Math.PI / 2, 0, 0); // Rotate so that the plane lied on XZ plane
     scene.add(plane);
 
+    const halfWidth = Math.round(this.width / 2);
+    const halfHeight = Math.round(this.height / 2);
     const centralXLineMaterial = new LineBasicMaterial({ color: 0xfa5252, opacity: 0.5 });
     const centralZLineMaterial = new LineBasicMaterial({ color: 0x228be6, opacity: 0.5 });
     const xLineGeometry = new BufferGeometry().setFromPoints([
-      new Vector3(-this.width, 0, 0),
-      new Vector3(this.width, 0 , 0),
+      new Vector3(-halfWidth, 0, 0),
+      new Vector3(halfWidth, 0 , 0),
     ]);
     const zLineGeometry = new BufferGeometry().setFromPoints([
-      new Vector3(0, 0, -this.height),
-      new Vector3(0, 0 , this.height),
+      new Vector3(0, 0, -halfHeight),
+      new Vector3(0, 0 , halfHeight),
     ]);
     const xLine = new Line(xLineGeometry, centralXLineMaterial);
     const zLine = new Line(zLineGeometry, centralZLineMaterial);
@@ -39,27 +50,25 @@ export class Ground {
     scene.add(zLine);
 
     const gridLineMaterial = new LineBasicMaterial({ color: 0xdee2e6 });
-    const xLinesNumber = (this.width / this.gridSize) * 2;
-    const zLinesNumber = (this.height / this.gridSize) * 2;
 
-    for (let x = -this.width; x < xLinesNumber; x += this.gridSize) {
+    for (let x = -halfWidth; x < halfWidth; x += this.gridSize) {
       if (x === 0) continue; // Do not render central line as it has been rendered earlier
 
       const gridLineGeometry = new BufferGeometry().setFromPoints([
-        new Vector3(x, 0, -this.width),
-        new Vector3(x, 0, this.width),
+        new Vector3(x, 0, -halfWidth),
+        new Vector3(x, 0, halfWidth),
       ]);
       const gridLine = new Line(gridLineGeometry, gridLineMaterial);
 
       scene.add(gridLine);
     }
 
-    for (let z = -this.height; z < zLinesNumber; z += this.gridSize) {
+    for (let z = -halfHeight; z < halfHeight; z += this.gridSize) {
       if (z === 0) continue; // Do not render central line as it has been rendered earlier
 
       const gridLineGeometry = new BufferGeometry().setFromPoints([
-        new Vector3(-this.height, 0, z),
-        new Vector3(this.height, 0, z),
+        new Vector3(-halfHeight, 0, z),
+        new Vector3(halfHeight, 0, z),
       ]);
       const gridLine = new Line(gridLineGeometry, gridLineMaterial);
 
